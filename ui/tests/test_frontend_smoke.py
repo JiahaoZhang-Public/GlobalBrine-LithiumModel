e77ed0385a28070330024c35ddad47b590b54845
Jiahao Zhang <jiahao.zhang.public@gmail.com>
2026-01-30 16:29:22 +0400

    remove: ui, version 0.1.0

--- ui/tests/test_frontend_smoke.py
+++
@@ -1,6 +0,0 @@
-def test_gradio_app_builds():
-    from ui.frontend.app import build_demo
-
-    demo = build_demo()
-    assert demo is not None
-
--- ui/tests/test_infer_light.py
+++
@@ -1,5 +0,0 @@
-def test_infer_light_not_configured(client):
-    r = client.get("/infer_light", params={"lat": 23.05, "lon": -67.25})
-    # Dummy mode doesn't implement light inference.
-    assert r.status_code in (404, 503)
-
